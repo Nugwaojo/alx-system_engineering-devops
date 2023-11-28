60019a0ddbf58460cfbc749a6a72aedcfa31435a
Nugwaojo <magiroyale21@gmail.com>
2023-11-28 15:19:09 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 #!/usr/bin/pup
 
-# Install an especific version of flask (2.1.0)
+# Install a specific version of flask (2.1.0)
 package { 'flask':
-ensure => '2.1.0',
-provider => 'pip3'
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
